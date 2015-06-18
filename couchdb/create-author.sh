d8922db8d8b58d647cc971bd33169a0934e4fdf9
jrsawvel <jrsawvel@gmail.com>
2015-06-18 18:15:39 -0400

    new doc and mod

--- couchdb/create-author.sh
+++ couchdb/create-author.sh
@@ -1,4 +1,7 @@
 
- curl -X POST -H "Content-Type: application/json" --data '{ "_id" : "JohnR", "type" : "author", "name" : "JohnR", "email" : "jothut@fastmail.fm", "current_session_id" : "-1" }' http://127.0.0.1:5984/veerydvlp1
+# decided to set the _id to the author name, therefore i may not need the 'name' field, but i'll leave it for now.
+ curl -X POST -H "Content-Type: application/json" --data '{ "_id" : "JohnR", "type" : "author", "name" : "JohnR", "email" : "x@x.com", "current_session_id" : "-1" }' http://127.0.0.1:5984/veerydvlp1
 
-# curl -X POST -H "Content-Type: application/json" --data '{ "type" : "author", "name" : "JR", "email" : "jothut@fastmail.fm", "current_session_id" : "-1" }' http://127.0.0.1:5984/veerydvlp1
+
+# not used
+# curl -X POST -H "Content-Type: application/json" --data '{ "type" : "author", "name" : "JR", "email" : "x@x.com", "current_session_id" : "-1" }' http://127.0.0.1:5984/veerydvlp1
